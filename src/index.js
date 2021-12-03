568390532368f98771d455a8304be9a69f423556
Kartik18g <gupta.kartik18kg@gmail.com>
2021-12-03 19:41:55 +0530

    auth routes

--- src/index.js
+++ src/index.js
@@ -2,11 +2,20 @@ import express from 'express'
 import dotenv from 'dotenv'
 import connectDB from './services/mongodb/connectDB'
 dotenv.config()
+import cors from 'cors'
+import authRoutes from './routes/authRoutes'
 
 const app = express()
 const port = process.env.PORT || 3003
 
 connectDB()
+
+app.use(cors())
+app.use(express.json())
+
+//route to handle auth requests
+app.use("/api/v1/auth", authRoutes)
+
 
 app.listen(port, (req, res) => {
     console.log(`Server listening at PORT ${port}`)
