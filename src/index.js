ca179f2e1eb4d7bd2d4705a449c301a76c981879
Hemanth <90907017+hemanthnaika@users.noreply.github.com>
2021-12-11 18:49:01 +0530

    heroku add

---
+++ src/index.js
@@ -0,0 +1,31 @@
+
+import express from 'express'
+import dotenv from 'dotenv'
+import connectDB from './services/mongodb/connectDB'
+dotenv.config()
+import cors from 'cors'
+import authRoutes from './routes/authRoutes'
+import categoryRoutes from './routes/categoryRoutes'
+import productRoutes from './routes/productRoutes'
+
+
+const app = express()
+const port = process.env.PORT || 3003
+
+connectDB()
+
+app.use(cors())
+app.use(express.json())
+
+//route to handle auth requests
+app.use("/api/v1/auth", authRoutes)
+app.use("/api/v1/category", categoryRoutes)
+app.use("/api/v1/product", productRoutes)
+
+app.get('/', (req, res) => {
+    res.send(`Server running at ${port}`)
+})
+
+app.listen(port, (req, res) => {
+    console.log(`Server listening at PORT ${port} (Deployed via workflow with secret)`)
+})
---
+++ src/services/mongodb/models/Review.js
@@ -0,0 +1,17 @@
+
+import mongoose from 'mongoose'
+
+const ReviewSchema = new mongoose.Schema({
+    authorName: {
+        type: String,
+        required: true
+    },
+    review: {
+        type: String,
+        required: true
+    }
+})
+
+const Review = mongoose.model('Review', ReviewSchema)
+
+export default Review
