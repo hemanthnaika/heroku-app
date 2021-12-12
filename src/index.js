61c2a2d119602c4ae30d50aba9cc8b4158ed7bbd
Hemanth <90907017+hemanthnaika@users.noreply.github.com>
2021-12-12 12:37:50 +0530

    index.js  Change

--- src/index.js
+++ src/index.js
@@ -23,7 +23,7 @@ app.use("/api/v1/category", categoryRoutes)
 app.use("/api/v1/product", productRoutes)
 
 app.get('/', (req, res) => {
-    res.send(`Server running at ${port} (Deployed via workflow with secret 12334)`)
+    res.send(`Server running at ${port} (Deployed via workflow with secret)`)
 })
 
 app.listen(port, (req, res) => {
